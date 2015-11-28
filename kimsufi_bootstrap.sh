ae0488c67720a3431c036d5c06bc4bfbe672d11f
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2015-11-28 20:59:54 +0100

    some further improvments

--- kimsufi_bootstrap.sh
+++ kimsufi_bootstrap.sh
@@ -4,6 +4,6 @@ ssh-keygen -R 91.121.164.117
 ssh-keygen -R the-captains-shack.com
 
 ssh-keyscan -t rsa,dsa the-captains-shack.com >> ~/.ssh/known_hosts
-ssh-keyscan -t 91.121.164.117 >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa,dsa 91.121.164.117 >> ~/.ssh/known_hosts
 
 ansible-playbook bootstrap.yml -u root --ask-pass -c ssh --limit kimsufi --ask-vault
